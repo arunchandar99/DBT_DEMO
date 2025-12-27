dd16424a9301f692d3d47eb64d17b7ffd3a6f097
arunchandar100@gmail.com <arunchandar100@gmail.com>
2025-12-27 17:47:57 +0000

    station_dimension model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,17 @@
+with bike as (
+
+select distinct
+start_station_id as station_id, 
+start_station_name as station_name, 
+start_lat as station_lat,
+start_lng as station_lng
+
+from {{ source('demo', 'bike') }}
+
+limit 10
+
+)
+
+select 
+* 
+from bike
